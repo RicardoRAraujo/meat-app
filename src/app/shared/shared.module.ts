0ca4b394a0f90a834b9a6f3d2b20932a849c434c
RicardoRAraujo <ricardorodriguesmg@gmail.com>
2018-04-23 14:53:57 -0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ModuleWithProviders } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { InputComponent } from './input/input.component'
+import { RadioComponent } from './radio/radio.component'
+import { RatingComponent } from './rating/rating.component'
+
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shoping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { SnackbarComponent } from "app/shared/messages/snackbar/snackbar.component";
+
+
+
+@NgModule({
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [InputComponent, RadioComponent, RatingComponent,
+            CommonModule, FormsModule, ReactiveFormsModule, SnackbarComponent]
+})
+
+export class SharedModule {
+  static forRoot(): ModuleWithProviders{
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    }
+  }
+}
